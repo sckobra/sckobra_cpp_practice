636ed935d664c843335c6f0da31c278d00aec2fe
Shreya Chati <shreyachati@cs.ucsb.edu>
2024-10-08 18:32:21 -0700

    working reverseList function

--- linkedListPractice.cpp
+++ linkedListPractice.cpp
@@ -15,6 +15,30 @@ Node* push_front(Node* h, int value){
   return myNodePointer;
 }
 
+void printList(Node* head){
+    Node* current = head;
+    while (current){
+        cout << "["<< current->data << "]";
+        current = current->next;
+        if(current) cout << "->";
+
+    }
+    cout <<endl;
+
+}
+
+Node* reverseList(Node* head){
+    Node* prev = nullptr;
+    Node* curr = head;
+    Node* next;
+    while (curr != nullptr){
+        next = curr->next;
+        curr->next = prev; //in the first iteration, this would make 40 node point to null
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
 
 
 int main(){
@@ -24,5 +48,7 @@ int main(){
     head = push_front(head, 30);
     head = push_front(head, 40);
     //printList(head);
+    printList(reverseList(head));
+    
     return 0;
 }
